9f3895e9c23882f29f50af8027f842bf2b748b86
Daenamu <dheodnr318@naver.com>
2020-07-27 23:41:20 +0900

    publish

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,7 @@ import gulp from "gulp"
 import sass from "gulp-sass" 
 import miniCSS from "gulp-csso"
 import ws from "gulp-webserver"
+import ghPages from "gulp-gh-pages"
 
 sass.compiler = require('node-sass')
 
@@ -25,7 +26,10 @@ const watch = () =>
 
 const webserver = () => gulp.src("build").pipe(ws({livereload:true, open:true}));
 
+const ghdeploy = () => gulp.src("build/**/*").pipe(ghPages())
+
 const assets = gulp.series([styles]);
 const live = gulp.parallel([webserver, watch]);
 
 export const dev = gulp.series([assets, live]);
+export const deploy = gulp.series([assets, ghdeploy]);
