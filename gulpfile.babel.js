91f3985bd896ce2dfc890c607d82659b9d1b35ad
Daenamu <dheodnr318@naver.com>
2020-07-27 19:45:22 +0900

    defalut setting complete

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,31 @@
 import gulp from "gulp"
+import sass from "gulp-sass" 
+import miniCSS from "gulp-csso"
+import ws from "gulp-webserver"
 
-export const dev = () => console.log("hi")
+sass.compiler = require('node-sass')
+
+const routes = {
+    scss: {
+        src:"src/scss/styles.scss",
+        dest: "build",
+        watch: "src/scss/*.scss"
+    }
+}
+
+const styles = () => 
+    gulp
+    .src(routes.scss.src)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(miniCSS())
+    .pipe(gulp.dest(routes.scss.dest));
+
+const watch = () =>
+    gulp.watch(routes.scss.watch, styles);
+
+const webserver = () => gulp.src("/").pipe(ws({livereload:true, open:true}));
+
+const assets = gulp.series([styles]);
+const live = gulp.parallel([webserver, watch]);
+
+export const dev = gulp.series([assets, live]);
